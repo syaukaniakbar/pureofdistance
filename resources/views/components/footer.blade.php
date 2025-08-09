0c299544f909245241fb286ef284d5797d1a9642
syaukaniakbar <syaukaniakbar2019@gmail.com>
2025-08-09 16:26:40 +0800

    update footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -19,7 +19,7 @@
           class="w-[190px] h-[60px] object-contain"
         >
         <!-- Address and Contact -->
-        <div class="text-left">
+        <!-- <div class="text-left">
           <div>
             <p class="font-medium text-slate-800">Address:</p>
             <p class="text-sm text-slate-600">Jl. KH. Wahid Hasyim 75119</p>
@@ -28,26 +28,16 @@
           <div class="mt-4">
             <p class="font-medium text-slate-800">Contact:</p>
             <a href="https://wa.me/6282254361507" class="text-sm text-slate-600">0822-5436-1507</p>
-            <a href="http://uwgm.ac.id" class="text-sm text-slate-600">uwgm.ac.id</a>
+            <a href="http://uwgm.ac.id" class="text-sm text-slate-600">pureofdistance.com</a>
           </div>
-        </div>
+        </div> -->
       </div>
       <div class="flex items-center w-full h-full mt-4 ml-4 md:mt-0 md:w-2/5">
         <div class="w-[400px] flex h-fit">
           <div class="flex flex-col w-2/5 h-full gap-4 py-2 ">
             <p class="font-bold text-slate-800">Pages</p>
             <a href="/" class="text-sm font-medium text-slate-800">Home</a>
-            <a href="/about" class="text-sm font-medium text-slate-800">About event</a>
-            <a href="/rules" class="text-sm font-medium text-slate-800">Rules</a>
-            <a href="https://uwgm.ac.id/" class="text-sm font-medium text-slate-800">Official website</a>
-            <a href="https://drive.google.com/file/d/1xgeZnC3Q0uplITVgnL3cfgo_rZQvAPjm/view?usp=sharing" class="text-sm font-medium text-slate-800">Surat Kuasa</a>
-            <a href="https://drive.google.com/file/d/1WDjn3wZR_oANiQ_J37Wgsuxqnez5TyTw/view?usp=sharing" class="text-sm font-medium text-slate-800">Terms and Condition</a>
-          </div>
-          <div class="flex flex-col w-2/5 h-full gap-4 py-2">
-            <p class="font-bold text-slate-800">Social Media</p>
-            <a href="https://www.instagram.com/uwgmofficial/" class="text-sm font-medium text-slate-800">Instagram</a>
-            <a href="https://x.com/ldkcesimauwgm" class="text-sm font-medium text-slate-800">Twitter</a>
-            <a href="https://www.facebook.com/uwgmofficial/" class="text-sm font-medium text-slate-800">Facebook</a>
+            <a href="/about" class="text-sm font-medium text-slate-800">Registration</a>
           </div>
         </div>
       </div>
