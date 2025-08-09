14cfd912de0167fc32cb4bf7bd5533742a7111f7
syaukaniakbar <syaukaniakbar2019@gmail.com>
2025-08-09 16:25:49 +0800

    update layout

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -9,6 +9,10 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
+   <link href="{{ asset('dist/css/lightbox.min.css') }}" rel="stylesheet">
+
+
 </head>
 
 <body class="font-sans antialiased">
@@ -25,6 +29,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/intersect@3.x.x/dist/cdn.min.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="{{ asset('dist/js/lightbox.min.js') }}"></script>
 </body>
 
 </html>
