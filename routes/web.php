8b64bbb2292a63a495585bde31138a3c4133c3f6
syaukaniakbar <syaukaniakbar2019@gmail.com>
2025-08-11 16:22:27 +0800

    make testing email

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegistrationController;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\RegistrationSuccess;
 
 Route::get('/', function () {
     return view('pages.home');
@@ -9,3 +11,26 @@ Route::get('/', function () {
 
 Route::get('/registration', [RegistrationController::class, 'create'])->name('register.create');
 Route::post('/registration', [RegistrationController::class, 'store'])->name('register.store');
+
+Route::get('/test-email', function () {
+    // Data dummy
+    $nama = 'Akhmad Syaukani Akbar';
+    $no_hp = '085219594240';
+    $jenis_kelamin = 'Laki-laki';
+    $email = 'cozycat2001@gmail.com'; // ganti dengan emailmu sendiri saat testing
+
+    // Kirim email
+    Mail::to($email)->send(new RegistrationSuccess($nama, $no_hp, $jenis_kelamin));
+
+    return 'Email test telah dikirim ke ' . $email;
+});
+
+Route::get('/preview-email', function () {
+    // Data dummy untuk preview
+    $nama = 'Akhmad Syaukani Akbar';
+    $no_hp = '085219594240';
+    $jenis_kelamin = 'Laki-laki';
+
+    // Tampilkan langsung di browser
+    return new RegistrationSuccess($nama, $no_hp, $jenis_kelamin);
+});
