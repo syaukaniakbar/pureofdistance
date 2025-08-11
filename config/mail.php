87cb14f48b1d07bef1b37cc93533d191567c20ac
syaukaniakbar <syaukaniakbar2019@gmail.com>
2025-08-11 16:23:10 +0800

    set up email name

--- config/mail.php
+++ config/mail.php
@@ -111,8 +111,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'pureofdistancee@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Pure of Distance'),
     ],
 
 ];
